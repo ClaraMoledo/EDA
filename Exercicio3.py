cb02bebbedb4b4cb98578169014903631a78e811
ClaraMoledo <anaclaramoledo@gmail.com>
2025-03-23 22:13:49 -0300

    Adicionar Exercicio3.py

--- Exercicio3.py
+++ Exercicio3.py
@@ -1,7 +1,18 @@
 import numpy as np  
 import matplotlib.pyplot as plt
 
-def f(x) 
+def f(x):
     return x**3 + x - 2 
 
-def  
+def metodo_bissecao (f, a, b, precisao):
+    if f(a) * f(b) >= 0:
+        return "Não existe raiz no intervalo"
+    
+    iteracoes = []
+    iteracao = 0
+    erro = b - a
+    
+    while erro > precisao:
+        iteracao += 1
+        m = ( a + b ) / 2  
+        
